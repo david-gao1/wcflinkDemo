1d9b16bb607ab3ff5a69e213050f9cc534d6be1b
roman.gao <roman.gao@cloudwise.com>
2022-01-06 19:24:55 +0800

    flink basic wordcount 理解流批处理的不同

---
+++ src/main/java/org/example/wordcount/WordCount.java
@@ -0,0 +1,43 @@
+package org.example.wordcount;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+/**
+ * 离线处理wordCount
+ */
+public class WordCount {
+    public static void main(String[] args) throws Exception {
+        // 创建执行环境
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        // 从文件中读取数据
+        String inputPath = "/Users/lianggao/MyWorkSpace/001project/wcflinkDemo/src/main/resources/flinkSQL.demo/0001baisicCDC.sql";
+        DataSet<String> inputDataSet = env.readTextFile(inputPath);
+
+        // 对数据集进行处理，按空格分词展开，转换成(word, 1)二元组进行统计
+        // 按照第一个位置的word分组
+        // 按照第二个位置上的数据求和
+        DataSet<Tuple2<String, Integer>> resultSet = inputDataSet
+                .flatMap(new MyFlatMapper())
+                .groupBy(0)
+                .sum(1);
+
+        resultSet.print();
+    }
+
+    public static class MyFlatMapper implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        @Override
+        public void flatMap(String s, Collector<Tuple2<String, Integer>> out) throws Exception {
+            String[] words = s.split(" ");
+            // 遍历所有word，包成二元组输出
+            for (String str : words) {
+                out.collect(new Tuple2<>(str, 1));
+            }
+        }
+    }
+
+}
